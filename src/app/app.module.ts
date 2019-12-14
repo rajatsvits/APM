ed97d06c9acb78552dcd95521cd2edb120f40400
IN22914337 <rajat.mahajan@tesco.com>
2019-12-15 00:31:24 +0530

    Added product details and routing paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { ProductListComponent } from './product/product-list.component';
 import { ConvertToSpacesPipes } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/start.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailsComponent } from './product/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -14,12 +17,23 @@ import { HttpClientModule } from '@angular/common/http';
     ProductListComponent,
     ConvertToSpacesPipes,
     StarComponent,
-
+    ProductDetailsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path : 'product', component : ProductListComponent },
+      { path : 'product/:id', component : ProductDetailsComponent },
+      { path : 'welcome', component : WelcomeComponent },
+      // Specify a default route to welcome component.
+      { path : '', redirectTo : 'welcome', pathMatch: 'full' },
+      // Wildcard path incase if the requested url doesn't match with any prior url. It is generally used to 
+      // display 404 not found page but here we are using to display welcome page
+      { path : '**', redirectTo : 'welcome', pathMatch: 'full' },
+    ])
   ],
   bootstrap: [AppComponent]
 })

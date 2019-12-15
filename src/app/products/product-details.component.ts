cd1cea7c15923a892fcee3d33f31a6bf02d4b95e
IN22914337 <rajat.mahajan@tesco.com>
2019-12-15 17:14:56 +0530

    added getproduct method

--- src/app/products/product-details.component.ts
+++ src/app/products/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-details.component.html',
@@ -8,26 +9,31 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   pageTitle : string = 'Product Details';
-  product:IProduct;
+  product:IProduct | undefined;
+  errorMessage : string = '';
 
   constructor( private route : ActivatedRoute,
-      private router : Router) { }
+      private router : Router,
+      private productService :ProductService) { }
 
   ngOnInit() {
     // Since parameter is a string so javascript provide a shortcut ie, use '+' to convert the string to numeric
     let id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += ` : ${id} `;
     // we could use http to access the desired product using this id.
-    this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+    console.log(id);
+    if(id){
+      this.getProduct(id);
     }
+    
+  }
+
+  getProduct(id : number) {
+    this.productService.getProduct(id).subscribe({
+      next : product => this.product = product,
+      error : err => this.errorMessage = err
+    })
+
   }
   onBack() : void {
     this.router.navigate(['/products']);
